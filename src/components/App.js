71742e1d48af269584477d9b1b17de3a05bc5c07
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-03-13 08:39:35 +0100

    add basic structure

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import '../styles/App.scss'
+
+const App = () => {
+  // States
+
+  // Events
+
+  // Functions to render
+
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default App;
+
